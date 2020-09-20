b81c21f101cd9da1fbfd4349f56564a0ab48a321
Lui Franco Rocha <lui@prontocombustiveis.com.br>
2020-09-19 22:50:03 -0300

    fix: firebase private key

--- src/services/db.ts
+++ src/services/db.ts
@@ -2,7 +2,7 @@ import admin from 'firebase-admin';
 
 const serviceAccount: { projectId: string, privateKey: string, clientEmail: string } = {
   clientEmail: process.env.CLIENT_EMAIL!,
-  privateKey: process.env.PRIVATE_KEY!,
+  privateKey: process.env.PRIVATE_KEY!.replace(/\\n/g, '\n'),
   projectId: process.env.PROJECT_ID!
 };
 
